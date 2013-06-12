c9a554542d25e5d20537e32c9ab9f4ad89e4abb7
Bryan Hickey <hello@happywidget.co>
2013-06-12 17:14:56 +1000

    added mail sign up thankyou

--- mail-signup-thankyou.php
+++ mail-signup-thankyou.php
@@ -1,6 +1,6 @@
 <?php include '_dochead.php'; ?>
 
-<title>Thanks for joining our mailing list | Grappling Tournaments Australia</title>
+<title>Mail Sign Up | Grappling Tournaments Australia</title>
 
 </head>
 <body>
@@ -8,8 +8,9 @@
 <?php include '_masthead.php'; ?>
 	<div class="row">
 		<div id="uniformRequirements" class="small-10 small-offset-1 text-content large-12 columns">
-			<h2>Thanks for joining our mailing list</h2>
-			<p></p>
+			<h2>Thanks for signing up</h2>
+			<p>Hey, thanks for signing up to our mailing list</p>
+			<p>We will be in touch when we have the next batch of exciting news to share.</p>
 		</div>
 	</div>
 
