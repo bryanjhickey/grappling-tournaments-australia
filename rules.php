c1e751f7afa112dfc1a027a88b2037271fbd3b14
Bryan Hickey <hello@happywidget.co>
2013-08-19 20:38:24 +1000

    fixing validation errors

--- rules.php
+++ rules.php
@@ -14,7 +14,7 @@
 			<p>All of GTA competitions follow the International Brazilian Jiu Jitsu Federations current rules. You can <a href="http://ibjjf.org/referees/rules/" target="_blank" title="Get the rule book from the IBJJF">download a PDF</a> of the rule book at their site.</p>
 			<p>Below is a video introduction of the rules governing our competition. Grab a beverage, some healthy snacks, sit down and watch.</p>
 			<div class="flex-video">
-				<iframe width="1280" height="720" src="//www.youtube.com/embed/ok2DWU0ZGg0" frameborder="0" allowfullscreen></iframe>
+				<iframe width="1280" height="720" src="//www.youtube.com/embed/ok2DWU0ZGg0" allowfullscreen></iframe>
 			</div>
 		</div>
 	</div>
@@ -23,5 +23,4 @@
 <?php include '_footer.php'; ?>
 
 </body>
-</div></body>
 </html>
