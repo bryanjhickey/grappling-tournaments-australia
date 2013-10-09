78f2919957fc0b25608aa2513ebd582a624a30e3
Bryan Hickey <hello@happywidget.co>
2013-10-09 14:36:23 +1100

    Working copy of results

--- 2013-gi.php
+++ 2013-gi.php
@@ -134,157 +134,160 @@
 						</thead>
 						<tbody>
 							<tr>
-								<td>White Super Feather (61.49 kg)</td>
-								<td>Kyle Sinclair</td>
-								<td>Neil Allen<br><span class="affiliation">Team Nemesis MMA</span></td>
-								<td>Sean Gauci<br><span class="affiliation">Dominance MMA</span></td>
+								<td>Rooster</td>
+								<td>Aaron Notarfrancesco<br><span class="affiliation">Australian Elite Team</span></td>
+								<td>Jake Savic<br><span class="affiliation">Australian Elite Team</span></td>
+								<td><br><span class="affiliation"></span></td>
 							</tr>
 							<tr>
-								<td>White Feather (67.49 kg)</td>
-								<td>Sang Yung Yap<br><span class="affiliation">Dominance MMA</span></td>
-								<td>Zack Hook<br><span class="affiliation">Dominance MMA</span></td>
-								<td>Emanuele Zanette<br><span class="affiliation">Maromba Academia</span></td>
+								<td>Light Feather</td>
+								<td>Rhishard Nakao<br><span class="affiliation">Dominance MMA</span></td>
+								<td>Caleb McCall<br><span class="affiliation">Australian Elite Team</span></td>
+								<td>Justin Barbuto<br><span class="affiliation">Dominance mma</span></td>
+								<td>Ryan Lee<br><span class="affiliation">Renegade MMA</span></td>
 							</tr>
 							<tr>
-								<td>White Light (73.49 kg)</td>
-								<td>Adam Watson<br><span class="affiliation">Australian Elite Team</span></td>
-								<td>Thomas Fricker<br><span class="affiliation">Dominance MMA</span></td>
-								<td>Peter Alavanja<br><span class="affiliation">Australian Elite Team</span></td>
+								<td>Feather</td>
+								<td>Justin Tile<br><span class="affiliation">Dominance MMA</span></td>
+								<td>Journ Tang<br><span class="affiliation">Peter de Been Jiu-Jitsu</span></td>
+								<td>Trung Le<br><span class="affiliation">Maromba Academia</span></td>
+								<td>Serhat Besnek<br><span class="affiliation">Maromba Academia</span></td>
 							</tr>
 							<tr>
-								<td>White Middle (79.49 kg)</td>
-								<td>Takunda Moyo<br><span class="affiliation">Xfc Carrum Downs</span></td>
-								<td>Mohammad Khawari<br><span class="affiliation">XFC Narre warren</span></td>
-								<td>Luke Jarvis<br><span class="affiliation">Infinite Mixed Martial Arts</span></td>
+								<td>Light</td>
+								<td>Matthew Cooper<br><span class="affiliation">Rise MMA</span></td>
+								<td>Harry Caldow<br><span class="affiliation">ISO Health</span></td>
+								<td>James McIlraith<br><span class="affiliation">Dominance MMA</span></td>
+								<td>Barry Eng<br><span class="affiliation">Checkmat</span></td>
 							</tr>
 							<tr>
-								<td>White Medium Heavy (85.49 kg)</td>
-								<td>Brock Beresford<br><span class="affiliation">Maromba Academia</span></td>
-								<td>Nicholas Mides<br><span class="affiliation">The Dojo</span></td>
+								<td>Medium</td>
+								<td>Ashley Woodcock<br><span class="affiliation">Peter de Been Jiu-Jitsu</span></td>
+								<td>Michael Fitt<br><span class="affiliation">The Dojo</span></td>
 								<td>James Londrigan<br><span class="affiliation">10th Planet Jiu Jitsu</span></td>
 							</tr>
 							<tr>
-								<td>White Heavy (91.49 kg)</td>
+								<td>Medium Heavy</td>
 								<td>Jess Smith<br><span class="affiliation">Ty Humphrey Jiu Jitsu</span></td>
 								<td>Daniel Schuardt<br><span class="affiliation">Extreme/Nova Uniao</span></td>
 								<td>Maseiga Tuala<br><span class="affiliation">XFC Narre Warren</span></td>
 							</tr>
 							<tr>
-								<td>White Super Heavy (97.49 kg)</td>
+								<td>White Super Heavy</td>
 								<td>Adam Reincke<br><span class="affiliation">Ultimate MMA</span></td>
 								<td>Dale Kent<br><span class="affiliation">Dominance MMA</span></td>
 								<td>Jason Stewart-Hesketh<br><span class="affiliation">Dominance MMA</span></td>
 							</tr>
 							<tr>
-								<td>White Ultra Heavy (OVER 97.49 kg)</td>
+								<td>White Ultra Heavy</td>
 								<td>Maka Tanginoa<br><span class="affiliation">Maromba Academia</span></td>
 								<td>George Tuuholoaki<br><span class="affiliation">Maromba Academia</span></td>
 								<td>Zlatko Bucman<br><span class="affiliation">Australian Elite Team</span></td>
 							</tr>
 							<tr>
-								<td>Blue Super Feather (61.49 kg)</td>
+								<td>Blue Feather</td>
 								<td>Ryan Ford<br><span class="affiliation">Peter de Been Jiu-Jitsu</span></td>
 								<td>Alex Sims<br><span class="affiliation">Iso Health</span></td>
 								<td>Gian Wickramaarachchi</td>
 							</tr>
 							<tr>
-								<td>Blue Feather (67.49 kg)</td>
+								<td>Blue Light</td>
 								<td>Richard Ivory<br><span class="affiliation">Carlson Gracie Team</span></td>
 								<td>Mamara Chong-Gum<br><span class="affiliation">Australian Elite Team</span></td>
 								<td>Luke Dufton<br><span class="affiliation">10 Planet Jiu Jitsu</span></td>
 							</tr>
 							<tr>
-								<td>Blue Light (73.49 kg)</td>
+								<td>Blue Medium</td>
 								<td>Joshua Flint<br><span class="affiliation">Checkmat</span></td>
 								<td>Nikki Thomas<br><span class="affiliation">Extreme/Nova Uniao</span></td>
 								<td>Tom Bishop<br><span class="affiliation">Infinite Mixed Martial Arts</span></td>
 							</tr>
 							<tr>
-								<td>Blue Middle (79.49 kg)</td>
+								<td>Blue Medium Heavy</td>
 								<td>Riley Raymond<br><span class="affiliation">Peter de Been Jiu-Jitsu</span></td>
 								<td>Jesse Arnott<br><span class="affiliation">Cherubin Jiu-Jitsu</span></td>
 								<td>Jason Dupont<br><span class="affiliation">Australian Elite Team</span></td>
 							</tr>
 							<tr>
-								<td>Blue Medium Heavy (85.49 kg)</td>
+								<td>Blue Heavy</td>
 								<td>Mitch O'Dowd<br><span class="affiliation">Cherubin Jiu-Jitsu</span></td>
 								<td>Luke Woodham<br><span class="affiliation">Extreme/Nova Uniao</span></td>
 								<td>Jeremy Wallert<br><span class="affiliation">Australian Elite Team</span></td>
 							</tr>
 							<tr>
-								<td>Blue Heavy (91.49 kg)</td>
-								<td>Aaron Dempsey<br><span class="affiliation">Peter de Been Jiu-Jitsu</span></td>
-								<td>Tobias Green</td>
-								<td>Sam Kehayias<br><span class="affiliation">Australian Elite Team</span></td>
-							</tr>
-							<tr>
-								<td>Blue Super Heavy (97.49 kg)</td>
+								<td>Blue Super Heavy</td>
 								<td>Sami Baki<br><span class="affiliation">Gracie Sydney</span></td>
 								<td>Chad Hatton<br><span class="affiliation">Maromba Academia</span></td>
 								<td>Jim Crute<br><span class="affiliation">Bendigo BJJ</span></td>
 							</tr>
 							<tr>
-								<td>Blue Ultra Heavy (OVER 97.49 kg)</td>
+								<td>Blue Ultra Heavy</td>
 								<td>Jadranko Jusufagic<br><span class="affiliation">Cia Paulista</span></td>
 								<td>Kim Barrett<br><span class="affiliation">80/20 BJJ</span></td>
 								<td></td>
 							</tr>
 							<tr>
-								<td>Purple Feather (67.49 kg)</td>
+								<td>Purple Feather</td>
 								<td>Kenneth Choo<br><span class="affiliation">Maromba Academia</span></td>
 								<td>Marco Ebeyer<br><span class="affiliation">Will Machado</span></td>
 								<td>Pratt Odisho<br><span class="affiliation">Australian Elite Team</span></td>
 							</tr>
 							<tr>
-								<td>Purple Light (73.49 kg)</td>
+								<td>Purple Light</td>
 								<td>Jake Matthews<br><span class="affiliation">Nexus Gracie Jiu-Jitsu</span></td>
 								<td>Aaran Hughes<br><span class="affiliation">Maromba Academia</span></td>
 								<td>Rod Costa<br><span class="affiliation">Mach 1 Fight Club</span></td>
 							</tr>
 							<tr>
-								<td>Purple Medium Heavy (85.49 kg)</td>
+								<td>Purple Medium</td>
 								<td>Callan Potter<br><span class="affiliation">Will Machado</span></td>
 								<td>Daniel Morandin<br><span class="affiliation">Maromba Academia</span></td>
 								<td>James Tomlinson<br><span class="affiliation">Cherubin Jiu-Jitsu</span></td>
 							</tr>
 							<tr>
-								<td>Purple Heavy (91.49 kg)</td>
+								<td>Purple Medium Heavy</td>
+								<td>Callan Potter<br><span class="affiliation">Will Machado</span></td>
+								<td>Daniel Morandin<br><span class="affiliation">Maromba Academia</span></td>
+								<td>James Tomlinson<br><span class="affiliation">Cherubin Jiu-Jitsu</span></td>
+							</tr>
+							<tr>
+								<td>Purple Heavy</td>
 								<td>William Abreu<br><span class="affiliation">FIGHT SPORTS</span></td>
 								<td>Ezra Trotter<br><span class="affiliation">Dominance MMA</span></td>
 								<td>Leigh Allwood<br><span class="affiliation">Maromba Academia</span></td>
 							</tr>
 							<tr>
-								<td>Purple Super Heavy (97.49 kg)</td>
-								<td>Arben Tairi<br><span class="affiliation">Peter de Been Jiu-Jitsu</span></td>
-								<td>Shaun Hurley<br><span class="affiliation">Dominance MMA</span></td>
-								<td>Darren Petty<br><span class="affiliation">Cherubin Jiu-Jitsu</span></td>
-							</tr>
-							<tr>
-								<td>Purple Ultra Heavy (OVER 97.49 kg)</td>
+								<td>Purple Ultra Heavy</td>
 								<td>Daniel Kelly</td>
 								<td>Jerome Maitland<br><span class="affiliation">Australian Elite Team</span></td>
 								<td>Ernest Lapornik<br><span class="affiliation">Gracie Sydney</span></td>
 							</tr>
 							<tr>
-								<td>Brown Feather (67.49 kg)</td>
+								<td>Brown Light</td>
 								<td>Michael Hourigan<br><span class="affiliation">Peter de Been Jiu-Jitsu</span></td>
 								<td>Frank Barca<br><span class="affiliation">10TH PLANET JJ MELBOURNE</span></td>
 								<td></td>
 							</tr>
 							<tr>
-								<td>Brown Middle (79.49 kg)</td>
+								<td>Brown Medium Heavy</td>
 								<td>Kane Gehling<br><span class="affiliation">Arruda BJJ</span></td>
 								<td>Lee Ting<br><span class="affiliation">Dominance MMA</span></td>
 								<td></td>
 							</tr>
 							<tr>
-								<td>Brown Medium Heavy (85.49 kg)</td>
+								<td>Brown Heavy</td>
 								<td>Kit Dale<br><span class="affiliation">Checkmat</span></td>
 								<td>Troy Skidmore<br><span class="affiliation">Dominance MMA</span></td>
 								<td></td>
 							</tr>
 							<tr>
-								<td>Brown Ultra Heavy (OVER 97.49 kg)</td>
+								<td>Black Medium Heavy</td>
+								<td>Paul Stolyar<br><span class="affiliation">Southside MMA Academy</span></td>
+								<td>Ben Patterson<br><span class="affiliation">Gracie Sydney</span></td>
+								<td>Leigh Burger<br><span class="affiliation">Submission Factory</span></td>
+							</tr>
+							<tr>
+								<td>Black Ultra Heavy</td>
 								<td>Paul Stolyar<br><span class="affiliation">Southside MMA Academy</span></td>
 								<td>Ben Patterson<br><span class="affiliation">Gracie Sydney</span></td>
 								<td>Leigh Burger<br><span class="affiliation">Submission Factory</span></td>
