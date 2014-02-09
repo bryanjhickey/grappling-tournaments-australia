c17f436c10abc9e14a4686ef676a36895b3b38cb
Bryan Hickey <hello@happywidget.co>
2014-02-10 08:07:30 +1100

    added results buttons to front page

--- index.php
+++ index.php
@@ -12,6 +12,9 @@
 		color: rgb(195, 137, 48);
 		text-shadow: 5px 5px 9px rgba(0, 0, 0, 0.6);
 	}
+	#officialRegister hr {
+		border-color: #fff;
+	}
 	</style>	
 </head>
 <body>
@@ -22,19 +25,20 @@
 		<div class="row">
 			<div class="large-12 small-12 small-centered large-centered columns">
 				<h2>THE 2014 AUSTRALIAN NO GI<br> JIU JITSU CHAMPIONSHIPS</h2>
-				<h6>STATE NETBALL AND HOCKEY CENTRE, 10 BRENS DRV, ROYAL PARK</h6>
-				<!--<a href="#" class="button medium radius" data-reveal-id="mailModal">LET'S DO IT</a>-->
-				<a href="http://www.strongvon.com/2014nogigta/m_results5.jsp?id=1442&indexstyle=109" class="button medium radius" title="View Weight Class Brackets" target="_blank">View Brackets</a>&nbsp;&nbsp;&nbsp;<a href="2014-no-gi.php" class="button medium radius" title="View Schedule">Weigh In Times</a>
-				<h6 class="hide-for-small">PLEASE NOTE: OPEN WEIGHT REGISTRATION MUST BE DONE IN PERSON<br>AFTER YOU HAVE SUCCESSFULLY COMPLETED YOUR WEIGHT DIVISION.</h6>
+<!-- 				<h6>STATE NETBALL AND HOCKEY CENTRE, 10 BRENS DRV, ROYAL PARK</h6>
+ -->				<!--<a href="#" class="button medium radius" data-reveal-id="mailModal">LET'S DO IT</a>-->
+				<a href="http://www.strongvon.com/2014nogigta/m_results5.jsp?id=1442&indexstyle=109" class="button medium radius" title="Bracket Results" target="_blank">Bracket Results</a>&nbsp;&nbsp;&nbsp;<a href="http://www.strongvon.com/2014nogigta/m_tournament_main.jsp?page=SummaryWinners.html" class="button medium radius" title="View Place winnerz" target="_blank">Place Winners</a>
+				<!--<h6 class="hide-for-small">PLEASE NOTE: OPEN WEIGHT REGISTRATION MUST BE DONE IN PERSON<br>AFTER YOU HAVE SUCCESSFULLY COMPLETED YOUR WEIGHT DIVISION.</h6> -->
 				<hr>
 				<div class="row hide-for-small">
 					<div id="timer" class="large-9 small-12 small-centered large-centered columns">
-					<h3 class="subheader">COMPETITION STARTS | 1 FEB &mdash; 9am</h3>
-						<div class="row">
+						<h3>NEXT COMPETITION:<br>
+						NOV 2014 (exact date TBA)</h3>
+						<!-- <div class="row">
 							<div class="large-10 large-centered small-12 columns"><h4><span id="timerhours" class="timebox">45 DAYS</span></h4></div>
-							<!-- <div class="large-4 small-4 columns"><h4><span id="timerminutes" class="timebox">58 MIN</span></h4></div>
-							<div class="large-4 small-4 columns"><h4><span id="timerseconds" class="timebox">58 SEC</span></h4></div> -->
-						</div>
+							<div class="large-4 small-4 columns"><h4><span id="timerminutes" class="timebox">58 MIN</span></h4></div>
+							<div class="large-4 small-4 columns"><h4><span id="timerseconds" class="timebox">58 SEC</span></h4></div>
+						</div> -->
 					</div>	
 				</div>
 			</div>
