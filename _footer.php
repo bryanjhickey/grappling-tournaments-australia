cdc201255142c7adf197a417073602169f029b45
Bryan Hickey <hello@happywidget.co>
2014-07-29 18:21:10 +1000

    removed AFC SUB from sponsors

--- _footer.php
+++ _footer.php
@@ -7,8 +7,7 @@
 			<div class="small-12 large-12 columns">
 				<h3 id="socialBanner">We Are One.</h3>
 				<div class="flex-video" style="padding-bottom: 40% !important;">
-<iframe src="//www.facebook.com/plugins/facepile.php?app_id=198476720177494&amp;href=https%3A%2F%2Fwww.facebook.com%2Fgrapplingtournaments.com.au&amp;action&amp;width&amp;height=200&amp;max_rows=10&amp;colorscheme=light&amp;size=large&amp;show_count=true&amp;appId=440269359360049" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:200px;" allowTransparency="true"></iframe>				</div>
-				<h3 id="socialAction">Join Forces Today</h3>
+					<div class="fb-facepile" data-app-id="198476720177494" data-href="https://www.facebook.com/grapplingtournaments.com.au" data-width="1000" data-height="200" data-max-rows="10" data-colorscheme="light" data-size="large" data-show-count="true"></div>				<h3 id="socialAction">Join Forces Today</h3>
 				<div class="row">
 					<div class="small-centered large-centered columns">
 						<div class="fb-like" data-href="https://www.facebook.com/grapplingtournaments.com.au" data-send="true" data-layout="button_count" data-width="150" data-show-faces="false" data-font="verdana"></div>
