4599ced5a8f69999cf61cf5306335848acccf573
Bryan Hickey <hello@happywidget.co>
2014-08-21 13:08:45 +1000

    minified images

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,10 +38,22 @@ module.exports = function(grunt) {
     //    },
     // },    
 
-
+      imagemin: {                          // Task
+        dynamic: {                         // Another target
+          files: [{
+            expand: true,                  // Enable dynamic expansion
+            cwd: 'source-images/',                   // Src matches are relative to this path
+            src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+            dest: 'assets/img/'                  // Destination path prefix
+          }]
+        }
+      },
 
     uglify: {
       dist: {
+        options: {
+          compress:true,
+        },
         files: [
           {'js/vendor/jquery.min.js': ['bower_components/jquery/dist/jquery.min.js']},
           {'js/vendor/fastclick.js': ['bower_components/fastclick/lib/fastclick.js']},
@@ -71,8 +83,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
 
   grunt.registerTask('build', ['sass']);
-  grunt.registerTask('default', ['copy', 'uglify', 'watch']);
+  grunt.registerTask('default', ['copy', 'uglify', 'imagemin', 'watch']);
 
 }
